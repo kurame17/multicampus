8ac31bc4e3dd290eafc4cd14433193da82b84f9e
kurame17 <jhsylee@gmail.com>
2017-12-11 13:22:58 +0900

    fuck

--- sinatra.rb
+++ sinatra.rb
@@ -10,6 +10,13 @@ get '/welcome' do
     send_file 'welcome.html'
 end
 
+#name은 파라미터
+get '/welcome/:name' do
+    @name = params[:name]   # 주소로부터 인수 전달받음.
+    erb :welcome    #  views 폴더의 welcome.erb로 @name 전달
+end
+
+#erb는 views 폴더에서 가져온다.
 get '/6' do
     erb :hello
 end
