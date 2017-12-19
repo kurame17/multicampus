27f6cd6843f2d21aeedcb6d09754af7c1e4a16be
kurame17 <jhsylee@gmail.com>
2017-12-19 14:42:49 +0900

    333

--- W6/secondapp/app/controllers/post_controller.rb
+++ W6/secondapp/app/controllers/post_controller.rb
@@ -20,4 +20,10 @@ class PostController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to '/'
+  end
 end
--- W6/secondapp/config/routes.rb
+++ W6/secondapp/config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get 'post/create'
   
   get 'post/show/:id' => 'post#show'  # id를 매개변수로 전달하며 컨트롤러의 show def로 보냄
+
+  get 'post/destroy/:id' => 'post#destroy'
 end
